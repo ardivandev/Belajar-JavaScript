d3d9a223b67599f4f2203f7371545b5657bc9720
Ardivan Nur Raihan Rahman <ardivannurraihanr@gmail.com>
2025-06-08 06:43:35 +0700

    update repo

---
+++ 01_variabel/script.js
@@ -0,0 +1,28 @@
+// * variabel
+/*
+- var, bisa digunakan diluar scope dan bisa diubah
+- let, bisa digunakan hanya di dalam scope dan bisa diubah
+- const, bisa digunakan hanya di dalam scope dan tidak bisa diubah
+*/
+
+if (true) {
+  // Scope
+  var nama = "Ardivan";
+  let umur = 17;
+  const nis = 192837;
+  umur = 19;
+  console.log("let : " + umur);
+  console.log("const : " + nis);
+}
+nama = "Boy";
+console.log("var : " + nama);
+
+// * Jawaban Tugas
+/*
+1. B
+2. B
+3. B
+4. B
+5. B
+6. B
+*/
