e46e3e88e0b9e57f17df49328da232914a2e2412
Ardivan Nur Raihan Rahman <ardivannurraihanr@gmail.com>
2025-06-08 19:42:14 +0700

    update repo

---
+++ 07_math_object/script.js
@@ -0,0 +1,36 @@
+// * Math Object
+// object bawaan yang berisi properti dan metode untuk melakukan operasi matematika
+
+// * Properti
+console.log(Math.PI);
+console.log(Math.E);
+
+// * Metode Math
+console.log(Math.abs(-10)); // 10
+console.log(Math.pow(10, 2)); // 100
+console.log(Math.sqrt(25)); // 5 (akar pangkat 2)
+console.log(Math.cbrt(27)); // 3 (akar pangkat 3)
+console.log(Math.max(1, 3, 4, 6, 8, 10, 11)); // mencari nilai tertinggi
+console.log(Math.min(1, 3, 4, 6, 8, 10, 11)); // mencari nilai terendah
+
+// * Pembulatan Angka
+console.log(Math.round(3.6)); // ke terdekat
+console.log(Math.floor(3.9)); // ke terkecil
+console.log(Math.ceil(3.3)); // ke atas
+console.log(Math.trunc(4.6)); // menghapus semua angka yang di belakang koma
+
+// * Random Number
+console.log(Math.random()); // antara 0 - 1
+console.log(Math.round(Math.random() * 100));
+
+// * Jawaban Tugas
+/*
+1. C
+2. B
+3. B
+4. D
+5. B
+6. A
+7. A
+8. C
+*/
